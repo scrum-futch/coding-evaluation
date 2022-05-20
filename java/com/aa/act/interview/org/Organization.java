548b5e91cf66dd7f579e7fc32ad5ef48f59a798c
scrum-futch <60011274+scrum-futch@users.noreply.github.com>
2022-05-19 22:36:03 -0700

    Hire method implement with DFS
    I added a helper method to keep the code clean, and one import to help implement DFS.

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,16 +1,17 @@
 package com.aa.act.interview.org;
 
 import java.util.Optional;
-
+import java.util.Stack;
 public abstract class Organization {
 
 	private Position root;
-	
+	private int employeeCount = 0;
 	public Organization() {
 		root = createOrganization();
 	}
 	
 	protected abstract Position createOrganization();
+	
 	
 	/**
 	 * hire the given person as an employee in the position that has that title
@@ -20,8 +21,19 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
-		return Optional.empty();
+		Optional<Position> pos = checkHire(root,person,title);
+		Stack<Position> dfs = new Stack<Position>();
+		dfs.push(root);
+		while(!dfs.empty()) {
+			for (Position p: dfs.pop().getDirectReports()) {
+				dfs.push(p);
+				if(pos.isEmpty()) 
+					pos = checkHire(p,person,title);
+				else
+					return pos;
+		}
+	}
+		return pos;
 	}
 
 	@Override
@@ -29,6 +41,17 @@ public abstract class Organization {
 		return printOrganization(root, "");
 	}
 	
+	//Helper function for the hire method
+	private Optional<Position> checkHire(Position p,Name person,String title) {
+		Optional<Position> pos = Optional.empty();
+		if(p.getTitle() == title && !p.isFilled()) {
+			Optional<Employee> newHire = Optional.of(new Employee(employeeCount,person));
+			p.setEmployee(newHire);
+			pos = Optional.of(p);
+			employeeCount++;
+			}
+		return pos;
+	}
 	private String printOrganization(Position pos, String prefix) {
 		StringBuffer sb = new StringBuffer(prefix + "+-" + pos.toString() + "\n");
 		for(Position p : pos.getDirectReports()) {
